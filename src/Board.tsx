cc1f2295a11f0ad0568e308b341e010ba0a2434c
Alex <alexrkoch@gmail.com>
2023-05-09 08:51:11 -0400

    fix css for game board

--- src/Board.tsx
+++ src/Board.tsx
@@ -7,18 +7,18 @@ function Board() {
   const board = useSelector((state: RootState) => state.game.board);
 
   return (
-    <div className="Board">
-      <div className="Row">
+    <div className="board">
+      <div className="row">
         <Square value={board[0]} index={0} />
         <Square value={board[1]} index={1} />
         <Square value={board[2]} index={2} />
       </div>
-      <div className="Row">
+      <div className="row">
         <Square value={board[3]} index={3} />
         <Square value={board[4]} index={4} />
         <Square value={board[5]} index={5} />
       </div>
-      <div className="Row">
+      <div className="row">
         <Square value={board[6]} index={6} />
         <Square value={board[7]} index={7} />
         <Square value={board[8]} index={8} />
